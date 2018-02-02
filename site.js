ac7cd0642608c58467cdd66c1e65fb70bc8f7e83
Zayaan Haider <Zayaan@Zayaans-MacBook-Pro.local>
2018-02-01 22:34:04 -0600

    wrote fucntion to check email

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
-$.noConflict();
+$('#form').on('submit',
+  function(e) {
+    var name = $('#name').val();
+    var phone = $('#phone').val();
+    var email = $('#email').val();
+    var trueName = false;
+    var truePhone = false;
+    var trueEmail = false;
+    var emailCheck =/.+@.+/;
+    e.preventDefault();
+
+    if (!checkEmail.test(email)) {
+      console.log( 'You did not put in a valid email');
+      return false;      
+    }
+  }
+)
