c56340a28557ba101bfd1bc9f7c15ff80a601746
Zayaan Haider <Zayaan@Zayaans-MacBook-Pro.local>
2018-02-18 22:41:36 -0600

    added name check for if length greater than 0 and email check with symbols

--- site.js
+++ site.js
@@ -8,13 +8,32 @@ $('#form').on('submit',
     var trueEmail = false;
     e.preventDefault();
 
+    if(trueName && trueEmail){
+      $(this).remove();
+      $('#description').append('<h1 id="thanks"> Thanks For Joining The Community!')
+      $('#description').append('<li> You will recive the letter at: ' + email.val + '</li>');
+
+    }
+
+    if(name.length > 0){
+      trueName =true;
+      $('#name-warning').remove();
+    }
+    else {
+      if(!$('#name-warning').length){
+        $('#name-get').append('<li id="name-warning">Enter A Valid Email Please!,/li>');
+      }
+    }
+
     if (emcheck.test(email))
     {
       trueEmail = true;
       #('#email-warning').remove();
     }
     else{
-      if()
+      if(!$('email-warning').length){
+        $('#email-get').append('<li id="email-warning"> Invalid Email Adress! Please enter a Valid Address<li>');
+      }
     }
   }
 )
