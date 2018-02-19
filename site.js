e85d4163d8d65f20c238c5d5774b572278e4bb60
Zayaan Haider <Zayaan@Zayaans-MacBook-Pro.local>
2018-02-18 21:38:30 -0600

    fixed intro paragraph to be mroe concise

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
+$.noConflict();
 $('#form').on('submit',
   function(e) {
     var name = $('#name').val();
-    var phone = $('#phone').val();
     var email = $('#email').val();
     var trueName = false;
     var truePhone = false;
@@ -11,7 +11,7 @@ $('#form').on('submit',
 
     if (!checkEmail.test(email)) {
       console.log( 'You did not put in a valid email');
-      return false;      
+      return false;
     }
   }
 )
