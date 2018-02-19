a0aeccdec5a7362a514d7d35d2a7be056014bd6f
Zayaan Haider <Zayaan@Zayaans-MacBook-Pro.local>
2018-02-18 21:46:06 -0600

    added randals regex to check for email

--- site.js
+++ site.js
@@ -3,15 +3,18 @@ $('#form').on('submit',
   function(e) {
     var name = $('#name').val();
     var email = $('#email').val();
+    var emcheck = /^[^\s@]+@[^\s@]+$/;
     var trueName = false;
-    var truePhone = false;
     var trueEmail = false;
-    var emailCheck =/.+@.+/;
     e.preventDefault();
 
-    if (!checkEmail.test(email)) {
-      console.log( 'You did not put in a valid email');
-      return false;
+    if (emcheck.test(email))
+    {
+      trueEmail = true;
+      #('#email-warning').remove();
+    }
+    else{
+      if()
     }
   }
 )
